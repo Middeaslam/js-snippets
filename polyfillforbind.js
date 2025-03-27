d879493f8e760077514b05b21f3ed55c53863bd6
Mahammad Aslam <middeaslam36@gmail.com>
2025-03-27 19:43:36 +0530

    function currying

--- polyfillforbind.js
+++ polyfillforbind.js
@@ -16,7 +16,7 @@ Function.prototype.myBind = function (...args) {
   const obj = this;
   const params = args.slice(1);
   return function (...args2) {
-    obj.apply(args[0], [params, ...args2]);
+    obj.apply(args[0], [...params, ...args2]);
   };
 };
 
