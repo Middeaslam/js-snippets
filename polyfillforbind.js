c441d45a7fe3ad34aaada7fdf5765d5df3077dd3
Mahammad Aslam <middeaslam36@gmail.com>
2025-03-27 19:17:40 +0530

    polyfill for bind

---
+++ polyfillforbind.js
@@ -0,0 +1,24 @@
+//pollyfil is a sort of browser fallback
+
+let name = {
+  firstName: 'Mahammad',
+  lastName: 'Aslam'
+};
+
+let printName = function (town, state) {
+  console.log(this.firstName + ' ' + this.lastName + ' ' + town + ' ' + state);
+};
+
+let printMyName = printName.bind(name, 'Hyd');
+printMyName();
+
+Function.prototype.myBind = function (...args) {
+  const obj = this;
+  const params = args.slice(1);
+  return function (...args2) {
+    obj.apply(args[0], [params, ...args2]);
+  };
+};
+
+let printMyName2 = printName.myBind(name, 'BGL');
+printMyName2('Karnataka');
